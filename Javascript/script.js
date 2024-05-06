763dfcc59c67077ae694b15d25926e58a33bf273
M.Arsalan <mohammadarsalanwarsi@gmail.com>
2024-05-07 03:34:40 +0500

    get username from api after login to home page

---
+++ Javascript/script.js
@@ -0,0 +1,18 @@
+let username = sessionStorage.getItem("username");
+let login_data = document.querySelector("#login_data");
+if (username == null) {
+  login_data.innerHTML = `<button type="button" class="btn btn-primary" id="sign_in">Sign in</button>`;
+  let sign_in = document.querySelector("#sign_in");
+  sign_in.addEventListener("click", () => {
+    window.location.assign("Login and Register/login.html");
+  });
+} else {
+  login_data.innerHTML = `<h6>Welcome: <i><span id="login_user" class="h5">${username}</span></i></h6>
+                <button type="button" class="btn btn-danger w-100" id="logout">Logout</button>
+               </div>`;
+  let logout = document.querySelector("#logout");
+  logout.addEventListener("click", () => {
+    sessionStorage.removeItem("username");
+    window.location.reload();
+  });
+}
