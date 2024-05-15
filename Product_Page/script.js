9e713d4a3a550957e82b229ce130916eb7d8fecb
M.Arsalan <mohammadarsalanwarsi@gmail.com>
2024-05-16 04:10:31 +0500

    producy page complete data in fetch.json remaining waiting for data

--- Product_Page/script.js
+++ Product_Page/script.js
@@ -17,22 +17,20 @@ if (username == null) {
   });
 }
 
-fetch("/Json/category.json")
+fetch("/Json/fetch.json")
   .then((res) => res.json())
   .then((json) => {
     var url = window.location.href;
     var product_id = url.substring(url.lastIndexOf("=") + 1);
     var data = "";
     let keys = Object.keys(json);
-    // console.log(keys);
+    console.log(keys);
     for (i in keys) {
       if (keys[i] == product_id) {
-        document.getElementById("breadcumbdata").innerHTML = keys[i];
+        // document.getElementById("breadcumbdata").innerHTML = keys[i];
         for (y in json[keys[i]]) {
-          data += `<div class="col-md-3 col-sm-12 col-lg-3" role="button">
-               <a href="/Product_Page/index.html?id=${
-                 json[keys[i]][y].id
-               }" class="text-decoration-none w-100 h-100" id="cardmain"><div class="card h-100 border-0" style="max-height:250px;min-height:100px;">
+          data += `<div class="col-md-6 col-sm-12 col-lg-3 m-lg-4 m-md-2 m-sm-2" role="button">
+               <div class="card h-100 p-4" style="max-height:450px;min-height:100px;">
                         <img src="${
                           json[keys[i]][y].img
                         }" class="card-img-top w-100 h-100" style="max-height:250px;min-height:100px;"  alt="${
@@ -40,13 +38,17 @@ fetch("/Json/category.json")
           } image">
                         <div class="card-body">
                            <h5 class="card-title text-dark text-decoration-none">${
-                             json[keys[i]][y].title
+                             json[keys[i]][y].heading
                            }</h5>
+                           <p>${json[keys[i]][y].sdes}</p>
+                           <a href="/Description_page/index.html?id=${
+                             json[keys[i]][y].id
+                           }" role="button" class="btn btn-outline-success">Learn More</a>
                         </div>
-                       </div></a>
+                       </div>
                     </div> `;
         }
       }
     }
-    document.getElementById("cat_data").innerHTML = data;
+    document.getElementById("product_data").innerHTML = data;
   });
