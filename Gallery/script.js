d8964545e91602e74bff35b1b2bb5fa057569018
M.Arsalan <mohammadarsalanwarsi@gmail.com>
2024-05-24 00:34:17 +0500

    gallery done 90%

--- Gallery/script.js
+++ Gallery/script.js
@@ -79,3 +79,91 @@ fetch(json_url)
 setTimeout(() => {
   document.querySelector("#spinloader").style.display = "none";
 }, 3000);
+let fetchurl = "/Json/fetch.json";
+fetch(fetchurl)
+  .then((res) => res.json())
+  .then((data) => {
+    let keys = Object.keys(data);
+    var catego = ` <option selected value="All Products">All Products</option>`;
+    for (i in keys) {
+      catego += `<option value="${keys[i]}">${keys[i]}</option>`;
+    }
+    document.getElementById("productcategory").innerHTML = catego;
+  });
+function categoryselect() {
+  let category = document.getElementById("productcategory").value;
+  if (category == "All Products") {
+    fetch(fetchurl)
+      .then((res) => res.json())
+      .then((data) => {
+        var gallery = "";
+        for (i in data) {
+          for (y in data[i]) {
+            gallery += ` <div class="col-md-3 col-lg-3 col-sm-12 cardcolumn">
+                    <a href="/Description_page/index.html?id=${data[i][y].id}&img=${data[i][y].img}&heading=${data[i][y].heading}&price=${data[i][y].price}&description=${data[i][y].mdes}"><div class="card h-100 shadow">
+                            <img src="${data[i][y].img}" class="card-img-top" alt="...">
+                          </div>
+                        </a>
+                </div>`;
+          }
+        }
+        document.querySelector(".galleydata").innerHTML = gallery;
+      });
+  } else {
+    fetch(fetchurl)
+      .then((res) => res.json())
+      .then((data) => {
+        var gallery = "";
+        let keys = Object.keys(data);
+        for (i in keys) {
+          if (keys[i] == category) {
+            for (y in data[keys[i]]) {
+              gallery += ` <div class="col-md-3 col-lg-3 col-sm-12 cardcolumn">
+              <a href="/Description_page/index.html?id=${
+                data[keys[i]][y].id
+              }&img=${data[keys[i]][y].img}&heading=${
+                data[keys[i]][y].heading
+              }&price=${data[keys[i]][y].price}&description=${
+                data[keys[i]][y].mdes
+              }"><div class="card h-100 shadow">
+                          <img src="${
+                            data[keys[i]][y].img
+                          }" class="card-img-top" alt="...">
+                        </div>
+                      </a>
+              </div>`;
+            }
+          }
+        }
+        document.querySelector(".galleydata").innerHTML = gallery;
+      });
+  }
+}
+
+fetch(fetchurl)
+  .then((res) => res.json())
+  .then((data) => {
+    var gallery = "";
+    for (i in data) {
+      for (y in data[i]) {
+        gallery += ` <div class="col-md-3 col-lg-3 col-sm-12 cardcolumn">
+                    <a href="/Description_page/index.html?id=${data[i][y].id}&img=${data[i][y].img}&heading=${data[i][y].heading}&price=${data[i][y].price}&description=${data[i][y].mdes}"><div class="card h-100 shadow">
+                            <img src="${data[i][y].img}" class="card-img-top" alt="...">
+                          </div>
+                        </a>
+                </div>`;
+      }
+    }
+    document.querySelector(".galleydata").innerHTML = gallery;
+  });
+let col3 = document.querySelector(".threecol");
+let col5 = document.querySelector(".threecol");
+let col12 = document.querySelector(".threecol");
+
+col3.addEventListener("click", () => {
+  let column = document.querySelectorAll("cardcolumn");
+  for (i = 0; i < column.length; i++) {
+    column[i].classList.remove("col-md-3 col-lg-3");
+    column[i].classList.add("col-md-12 col-lg-12");
+  }
+});
