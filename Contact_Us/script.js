dd6d06ac13d3fe5d43a3b2eade15782e247d3d7b
Saqib Ahmed <153153100+CodewithSaqib@users.noreply.github.com>
2024-05-23 12:23:52 -0700

    Validation and api updated

--- Contact_Us/script.js
+++ Contact_Us/script.js
@@ -26,4 +26,43 @@ let addressinput = document.querySelector(".addressinput");
 
 submitbtn.addEventListener("click", (e) => {
   e.preventDefault();
+  if(nameinput.value =="" || emailinput.value=="" || numberinput.value=="" || message.value=="" || addressinput.value=="" ){
+    alert("Please fill all the fields");
+                return false;
+  }
+  if (!nameinput.value.match(/[A-z]/)) {
+    alert("Name can only contain letters and spaces");
+    return false;
+}
+if (!emailinput.value.match(/[A-z]+[0-9]+[@]+[a-z]{5}[.]+[a-z]{3}/)) {
+  alert("Please enter a valid email address");
+  return false;
+}
+if (!numberinput.value.match(/[+92][0-9]{12}/)) {
+  alert("Please enter a pakistani mobile number");
+  return false;
+}
+if (!addressinput.value.match(/[A-z]/)) {
+  alert("Please enter a Valid Adress");
+  return false;
+}
+if (!addressinput.value.match(/[A-z]/)) {
+  alert("Please write a message");
+  return false;
+}
+
+
+
+Email.send({
+  Host : "smtp.elasticemail.com",
+  Username : "saqibkhan4567892@gmail.com",
+  Password : "9F60F62D966FA23CF9465C50B7D341C32CB4",
+  To : 'saqibkhan4567892@gmail.com',
+  From : document.getElementById("email").value,
+  Subject : "New Contact Form Enquiry",
+  Body : "And this is the body"
+}).then(
+message => alert("Form Submit Sucessfully")
+);
+
 });
